fec11499c9485e539aa2db87adfa9fe1e6608804
verabunny321@gmail.com <verabunny321@gmail.com>
2024-04-18 18:21:29 -0400

    clean up code to fix pylint issue and use enum to replace raw string

--- extract_data.py
+++ extract_data.py
@@ -1,46 +1,48 @@
-import sys
+"""_summary_
+"""
+from enum import Enum
 import pandas as pd
 import yfinance as yf
 
+class StockData(Enum):
+    """_summary_
 
-stock = sys.argv[1]
-ticker = yf.Ticker(stock)
+    Args:
+        Enum (_type_): _description_
+    """    
+    DATE = 'date'
+    STOCKSPLITS = 'Stock Splits'
+    DIVIDENDS = 'Dividends'
+    STOCK = 'stock'
 
-def get_stock_history(stock:str) -> pd.DataFrame:
+def get_stock_history(stock:str):
     """_summary_
     Args:
         stock (str): _description_
     Returns:
         pd.DataFrame: _description_
     """
+    ticker = yf.Ticker(stock)
     result = ticker.history(period="6d")
     result.reset_index(inplace = True)
-    result.drop(columns = ['Stock Splits'], inplace = True)
-    result['Dividends'] = result['Dividends'].astype(int)
+    result.drop(columns = [StockData.STOCKSPLITS.value], inplace = True)
+    result[StockData.DIVIDENDS.value] = result[StockData.DIVIDENDS.value].astype(int)
     result.columns = result.columns.str.lower()
-    result['stock'] = stock
-    result['date'] = result['date'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S.%f'))
+    result[StockData.STOCK.value] = stock
+    result[StockData.DATE.value] = result[StockData.DATE.value].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S.%f'))
     return result
 
-
-history = get_stock_history(stock)
-print(history)
-
-def get_stock_financials(stock:str) -> pd.DataFrame:
+def get_stock_financials(stock:str):
     """_summary_
     Args:
         stock (str): _description_
     Returns:
         pd.DataFrame: _description_
     """
+    ticker = yf.Ticker(stock)
     shares = ticker.income_stmt
     shares = shares.iloc[:17].T
     shares.reset_index(inplace = True)
-    shares = shares.rename(columns = {'index' : 'date'})
-    shares['date'] = shares['date'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S.%f'))
+    shares = shares.rename(columns = {'index' : StockData.DATE.value})
+    shares[StockData.DATE.value] = shares[StockData.DATE.value].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S.%f'))
     return shares
-
-finance = get_stock_financials(stock)
-print(finance)
-
-
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import pandas as pd
-from typing import List,Dict
+from extract_data import get_stock_history, get_stock_financials
 
+def main():
+    """_summary_
+    """
+    stock = 'MSFT'
+    print(get_stock_history(stock))
+    print(get_stock_financials(stock))
+
+if __name__ == "__main__":
+    main()
