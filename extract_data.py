2862fbfec88b6188cf3fe99d58deb39a8535db51
verabunny321@gmail.com <verabunny321@gmail.com>
2024-04-18 18:14:17 -0400

    message

--- extract_data.py
+++ extract_data.py
@@ -1,11 +1,18 @@
+import sys
 import pandas as pd
 import yfinance as yf
-import sys
+
 
 stock = sys.argv[1]
 ticker = yf.Ticker(stock)
 
-def get_stock_history(stock:str) -> pd.DataFrame:  
+def get_stock_history(stock:str) -> pd.DataFrame:
+    """_summary_
+    Args:
+        stock (str): _description_
+    Returns:
+        pd.DataFrame: _description_
+    """
     result = ticker.history(period="6d")
     result.reset_index(inplace = True)
     result.drop(columns = ['Stock Splits'], inplace = True)
@@ -20,6 +27,12 @@ history = get_stock_history(stock)
 print(history)
 
 def get_stock_financials(stock:str) -> pd.DataFrame:
+    """_summary_
+    Args:
+        stock (str): _description_
+    Returns:
+        pd.DataFrame: _description_
+    """
     shares = ticker.income_stmt
     shares = shares.iloc[:17].T
     shares.reset_index(inplace = True)
