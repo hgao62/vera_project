c1e0c33573b59f8d0c2b5c49e99a7e4585db46f9
verabunny321@gmail.com <verabunny321@gmail.com>
2024-04-18 03:57:41 -0400

    message

---
+++ extract_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import yfinance as yf
+import sys
+
+stock = sys.argv[1]
+ticker = yf.Ticker(stock)
+
+def get_stock_history(stock:str) -> pd.DataFrame:  
+    result = ticker.history(period="6d")
+    result.reset_index(inplace = True)
+    result.drop(columns = ['Stock Splits'], inplace = True)
+    result['Dividends'] = result['Dividends'].astype(int)
+    result.columns = result.columns.str.lower()
+    result['stock'] = stock
+    result['date'] = result['date'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S.%f'))
+    return result
+
+
+history = get_stock_history(stock)
+print(history)
+
+def get_stock_financials(stock:str) -> pd.DataFrame:
+    shares = ticker.income_stmt
+    shares = shares.iloc[:17].T
+    shares.reset_index(inplace = True)
+    shares = shares.rename(columns = {'index' : 'date'})
+    shares['date'] = shares['date'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S.%f'))
+    return shares
+
+finance = get_stock_financials(stock)
+print(finance)
+
+
