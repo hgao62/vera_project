99bbe520c99d5b797e456058eeb02d0a285f29c5
Kobe Gao <hgao62@uwo.ca>
2024-04-26 16:42:42 -0400

    test commit of pylint

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from extract_data import get_stock_history, get_stock_financials, get_news, get_exchange_rate
+from extract_data import (
+    get_stock_history,
+    get_stock_financials,
+    get_news,
+    get_exchange_rate,
+)
+
 
 def main():
-    """_summary_
-    """
-    stock = 'TSLA'
+    """_summary_"""
+    stock = "TSLA"
     print(get_stock_history(stock))
     print(get_stock_financials(stock))
-    rate = get_exchange_rate(stock, '5d', '1d', 'GBP')
+    rate = get_exchange_rate(stock, "5d", "1d", "GBP")
     print(rate)
     news = get_news(stock)
     print(news)
 
 
 if __name__ == "__main__":
+    # test commit
     main()
