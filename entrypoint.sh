44206404565565fe1602d5249c4454761805b50d
verabunny321@gmail.com <verabunny321@gmail.com>
2024-05-16 18:11:06 -0400

    removed echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,4 @@
 airflow db init
 airflow users create --username admin --password admin --firstname Vera --lastname Zhao --role Admin --email verabunny321@gmail.com
 airflow webserver -p 8080
-ECHO "Airflow webserver is running"
-ECHO "About to run Airflow scheduler"
 airflow scheduler
-ECHO "irflow scheduler is running"
